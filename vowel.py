bbba8065afc9ca195d71c64223c4e8024f635e48
Haridharan25 <41145730+Haridharan25@users.noreply.github.com>
2018-07-12 15:47:06 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -2,6 +2,6 @@ n=input(" ")
 if n in ('a', 'e', 'i', 'o', 'u'):
     print ("Vowel")
 elif n in ('b', 'c', 'd', 'f', 'g','h', 'j', 'k', 'l', 'm','n', 'p', 'q', 'r', 's','t', 'v', 'w', 'x', 'y', 'z'):
-    print ("Constant")
+    print ("Consonant")
 else:
     print ("Invalid")
