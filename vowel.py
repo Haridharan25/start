72bd6e635c3cfa9880c5e1a63e1b1c7e26abd979
Haridharan25 <41145730+Haridharan25@users.noreply.github.com>
2018-07-12 15:44:13 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+n=input(" ")
+if n in ('a', 'e', 'i', 'o', 'u'):
+    print ("Vowel")
+elif n in ('b', 'c', 'd', 'f', 'g','h', 'j', 'k', 'l', 'm','n', 'p', 'q', 'r', 's','t', 'v', 'w', 'x', 'y', 'z'):
+    print ("Constant")
+else:
+    print ("Invalid")
